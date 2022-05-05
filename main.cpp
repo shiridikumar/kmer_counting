09f7e0947ec801711ced2ce8c02f508a889bb17c
shiridikumar <p.shiridi@research.iiit.ac.in>
2022-05-05 19:28:11 +0530

    Pthread code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,67 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <ctime>
+#include <pthread.h>
 
 using namespace std;
 
+string DNA_sequence;
+map<string, int> kmer;
+int k = 4;
+void *thread_call(void* i){
+    long tid;
+     tid = (long)i;
+    cout<<"ehllo "<<tid <<endl;
+    int base=tid;
+    for(int i=0;i<DNA_sequence.length();i+=k){
+        kmer[DNA_sequence.substr(i,k)]+=1;
+    }
+    cout<<kmer.size()<<"***********"<<endl;
+    return 0;
+}
 
-int main(int argc, char **argv) {
-    map <string,int> kmer;
+
+int main(int argc, char **argv)
+{
     ifstream input(argv[1]);
-    if (!input.good()) {
+    if (!input.good())
+    {
         std::cerr << "Error opening: " << argv[1];
         return -1;
     }
-    string line, id, DNA_sequence;
-      while (std::getline(input, line)) {
-        if(line.empty())
+    string line, id;
+    while (std::getline(input, line))
+    {
+        if (line.empty())
             continue;
 
-        if (line[0] != '>') {
+        if (line[0] != '>')
+        {
             DNA_sequence += line;
         }
     }
-    cout<<DNA_sequence.length();
+     time_t start, end;
+     time(&start);
 
+    // for (int i = 0; i < DNA_sequence.length(); i ++)
+    // {
+    //     // cout<<DNA_sequence.substr(i,k)<<endl;
+    //     kmer[DNA_sequence.substr(i, k)] += 1;
+    // }
+    // cout << kmer.size() << endl;
+    // cout << (float)(clock() - start) / CLOCKS_PER_SEC << endl;
+
+    pthread_t threads[100];
+    pthread_t ptid;
+    for(int i=0;i<k;i++){
+         pthread_create(&threads[i], NULL, thread_call, (void *)i);
+    }
+    for(int i=0;i<k;i++){
+         pthread_join(threads[i], NULL);
+    }
+    time(&end);
+    cout << (double)(end-start) << endl;
+   
+
+    // int rc;
 }
